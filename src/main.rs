1e1268eaee6dc1928175a6f4e0ec926f8760f81b
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-17 08:44:52 +0200

    Add up and down keys

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,57 @@
 use colored::Colorize;
-use std::env;
-use std::io::{stdin, stdout, Write, Error};
+use std::io::{Error, Write};
+use std::time::Duration;
+use std::{env, process, thread};
+use console::{Term, Key};
 use std::path::Path;
 use std::process::Command;
+
+// Search
+// History
 fn main() {
     loop {
         let command = get_command();
-        if command.is_empty() {
-            continue;
-        }
-
-        let mut parts = command.split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
 
         match command {
-            "cd" => {
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprint(e);
+            Action::Down => {
+                println!("Got down arrow")
+            }
+            Action::Up => {
+                println!("Got up arrow")
+            }
+
+            Action::Command(command) => {
+                if command.is_empty() {
+                    continue;
+                }
+
+                let mut parts = command.split_whitespace();
+                let command = parts.next().unwrap();
+                let args = parts;
+
+                match command {
+                    "cd" => {
+                        let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                        let root = Path::new(new_dir);
+                        if let Err(e) = env::set_current_dir(&root) {
+                            eprint(e);
+                        }
+                    }
+                    "exit" => break,
+                    command => match Command::new(command).args(args).spawn() {
+                        Ok(mut child) => {
+                            child.wait().unwrap();
+                        }
+                        Err(e) => eprint(e),
+                    },
                 }
             }
-            command => match Command::new(command).args(args).spawn() {
-                Ok(mut child) => {
-                    child.wait().unwrap();
-                }
-                Err(e) => eprint(e)
-            },
         }
     }
 }
 
-fn get_command() -> String {
+fn get_command() -> Action {
+    let mut term = Term::stdout();
     let path = env::current_dir().unwrap().display().to_string();
     let crr_dir = path.split("/").last().unwrap();
     let path = format!("{}", crr_dir).bold().yellow().underline();
@@ -40,15 +59,35 @@ fn get_command() -> String {
         .bold()
         .underline()
         .green();
-    print!("{}", format!("{} {} ##>", prefix, path).bold().cyan());
-    stdout().flush().unwrap();
-    let mut buf = String::new();
-    stdin().read_line(&mut buf).unwrap();
+    let prefix =  format!("{} {} ##>", prefix, path).bold().cyan();
 
-    buf.trim().to_string()
+    term.write(format!("{}", prefix).as_bytes()).unwrap();
+
+    let mut command = String::new();
+    loop {
+        let key = term.read_key().unwrap();
+        
+        match key {
+            Key::ArrowUp => return Action::Up,
+            Key::ArrowDown => return Action::Down,
+            Key::Enter => {break;}
+            Key::Char(c) => {
+                command.push(c);
+                term.write_all(&String::from(c).as_bytes()).unwrap();
+            }
+            _ => {}
+        }
+    }
+    println!("\n");
+    Action::Command(command)
 }
-
 
 fn eprint(e: Error) {
     println!("{}", format!("Error: {e}").bold().on_red())
+}
+
+enum Action {
+    Up,
+    Down,
+    Command(String),
 }
